8b09d62df5a8826e431223e81cab6b646a0b792b
Dominik Jain <djain@gmx.net>
2020-03-14 00:14:07 +0100

    Added some classification tests

---
+++ test/classification.py
@@ -0,0 +1,42 @@
+import logging
+
+import sklearn.datasets
+import pandas as pd
+
+from sensai import InputOutputData, VectorClassificationModel, VectorClassificationModelEvaluator
+
+
+log = logging.getLogger(__name__)
+
+
+class IrisDataSet:
+    _iod = None
+
+    @classmethod
+    def getInputOutputData(cls):
+        if cls._iod is None:
+            d = sklearn.datasets.load_iris()
+            inputs = pd.DataFrame(d["data"], columns=d["feature_names"])
+            targetNames = d["target_names"]
+            outputs = pd.DataFrame({"class": [targetNames[x] for x in d["target"]]})
+            cls._iod = InputOutputData(inputs, outputs)
+        return cls._iod
+
+
+class ClassificationTestCase:
+    def __init__(self, data: InputOutputData):
+        self.data = data
+
+    def testMinAccuracy(self, model: VectorClassificationModel, minAccuracy: float):
+        ev = VectorClassificationModelEvaluator(self.data, testFraction=0.2)
+        ev.fitModel(model)
+        resultData = ev.evalModel(model)
+        stats = resultData.getEvalStats()
+        #stats.plotConfusionMatrix().savefig("cmat.png")
+        log.info(f"Results for {model.getName()}: {stats}")
+        assert stats.getAccuracy() >= minAccuracy
+
+
+class IrisClassificationTestCase(ClassificationTestCase):
+    def __init__(self):
+        super().__init__(IrisDataSet.getInputOutputData())
