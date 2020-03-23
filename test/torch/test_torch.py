59c4b6a10bd8c692ea279b9df65ec5ee3788fc63
Dominik Jain <dominik.jain@jambit.com>
2020-03-23 19:46:56 +0100

    Fixed reference to renamed class in test

--- test/torch/test_torch.py
+++ test/torch/test_torch.py
@@ -1,4 +1,3 @@
-import logging
 import re
 
 import sklearn
@@ -13,6 +12,6 @@ import sensai.torch
 def test_MLPClassifier():
     featureNames = IrisDataSet.getInputOutputData().inputs.columns
     dftNorm = DFTNormalisation([DFTNormalisation.Rule(re.escape(f)) for f in featureNames], defaultTransformerFactory=sklearn.preprocessing.StandardScaler)
-    model = sensai.torch.models.TorchMultiLayerPerceptronVectorClassificationModel(hiddenDims=(50,25,8), cuda=False, epochs=1000, optimiser="adam",
+    model = sensai.torch.models.MultiLayerPerceptronVectorClassificationModel(hiddenDims=(50,25,8), cuda=False, epochs=1000, optimiser="adam",
         batchSize=200, normalisationMode=NormalisationMode.NONE, hidActivationFunction=torch.tanh).withName("torchMLPClassifier").withInputTransformers([dftNorm])
     IrisClassificationTestCase().testMinAccuracy(model, 0.9)
