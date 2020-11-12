21b338af43f91fd0649567b25dee8e7707ee21e1
Michael Panchenko <michael.panchenko@unternehmertum.de>
2020-11-12 14:00:15 +0100

    Exposed creation methods in evaluation init

--- src/sensai/evaluation/__init__.py
+++ src/sensai/evaluation/__init__.py
@@ -1,6 +1,6 @@
 from .crossval import VectorClassificationModelCrossValidator, VectorRegressionModelCrossValidator, \
     VectorClassificationModelCrossValidationData, VectorRegressionModelCrossValidationData
 from .eval_util import RegressionEvaluationUtil, ClassificationEvaluationUtil, MultiDataEvaluationUtil, \
-    evalModelViaEvaluator
+    evalModelViaEvaluator, createEvaluationUtil, createVectorModelEvaluator, createVectorModelCrossValidator
 from .evaluator import VectorClassificationModelEvaluator, VectorRegressionModelEvaluator, \
     VectorRegressionModelEvaluationData, VectorClassificationModelEvaluationData
