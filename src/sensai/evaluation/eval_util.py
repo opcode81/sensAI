f192d4c6ef81f97dc4c130c27fc755e54845180f
Michael Panchenko <michael.panchenko@unternehmertum.de>
2021-01-20 16:54:46 +0100

    Added evaluators for rule based models. Also a renaming (breaking change)
    Renamed the base class VectorModelEvaluator to PredictorModelEvaluator.
    This should be fine since probably no user inherits from VectorModelEvaluator directly

--- src/sensai/evaluation/eval_util.py
+++ src/sensai/evaluation/eval_util.py
@@ -21,7 +21,7 @@ from .crossval import PredictorModelCrossValidationData, VectorRegressionModelCr
 from .eval_stats.eval_stats_base import EvalStats, EvalStatsCollection
 from .eval_stats.eval_stats_classification import ClassificationEvalStats
 from .eval_stats.eval_stats_regression import RegressionEvalStats
-from .evaluator import VectorModelEvaluator, PredictorModelEvaluationData, VectorRegressionModelEvaluator, \
+from .evaluator import PredictorModelEvaluator, PredictorModelEvaluationData, VectorRegressionModelEvaluator, \
     RegressionModelEvaluationData, VectorClassificationModelEvaluator, ClassificationModelEvaluationData
 from ..data_ingest import InputOutputData
 from ..util.io import ResultWriter
@@ -32,7 +32,7 @@ log = logging.getLogger(__name__)
 TModel = TypeVar("TModel", bound=VectorModel)
 TEvalStats = TypeVar("TEvalStats", bound=EvalStats)
 TEvalStatsCollection = TypeVar("TEvalStatsCollection", bound=EvalStatsCollection)
-TEvaluator = TypeVar("TEvaluator", bound=VectorModelEvaluator)
+TEvaluator = TypeVar("TEvaluator", bound=PredictorModelEvaluator)
 TCrossValidator = TypeVar("TCrossValidator", bound=VectorModelCrossValidator)
 TEvalData = TypeVar("TEvalData", bound=PredictorModelEvaluationData)
 TCrossValData = TypeVar("TCrossValData", bound=PredictorModelCrossValidationData)
