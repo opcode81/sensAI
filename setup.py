cd3788edeedd3ac901b649d9aae09aac6ded3be5
Michael Panchenko <michael.panchenko@unternehmertum.de>
2021-02-18 16:19:22 +0100

    Bump version: 0.0.8 → 0.0.9.dev0

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ setup(
     package_dir={"": "src"},
     packages=find_packages(where="src"),
     include_package_data=True,
-    version='0.0.8',
+    version='0.0.9.dev0',
     description='Library for sensible AI',
     install_requires=[
         line
