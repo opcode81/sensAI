e918acd6dd1f1ea566a22eb191245f09e99f906a
Dominik Jain <djain@gmx.net>
2021-06-17 21:08:41 +0200

    Improved formatting

--- tests/frameworks/torch/test_torch.py
+++ tests/frameworks/torch/test_torch.py
@@ -19,7 +19,10 @@ def test_MLPClassifier(irisDataSet, irisClassificationTestCase, testResources):
     featureNames = irisDataSet.getInputOutputData().inputs.columns
     dftNorm = DFTNormalisation([DFTNormalisation.Rule(re.escape(f)) for f in featureNames], defaultTransformerFactory=sklearn.preprocessing.StandardScaler)
     model = sensai.torch.models.MultiLayerPerceptronVectorClassificationModel(hiddenDims=(50,25,8), cuda=False, epochs=100, optimiser="adam",
-        batchSize=200, normalisationMode=NormalisationMode.NONE, hidActivationFunction=torch.tanh).withName("torchMLPClassifier").withInputTransformers([dftNorm]).withFeatureGenerator(FeatureGeneratorTakeColumns())
+            batchSize=200, normalisationMode=NormalisationMode.NONE, hidActivationFunction=torch.tanh) \
+        .withName("torchMLPClassifier") \
+        .withInputTransformers([dftNorm]) \
+        .withFeatureGenerator(FeatureGeneratorTakeColumns())
     irisClassificationTestCase.testMinAccuracy(model, 0.8)
 
 
