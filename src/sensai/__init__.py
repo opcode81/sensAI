f1b86677bf9797b82581f25f2a3d235a4c1a0632
Dominik Jain <dominik.jain@jambit.com>
2022-02-22 11:17:17 +0100

    Bump version: 0.1.7.dev0 → 0.1.7

--- src/sensai/__init__.py
+++ src/sensai/__init__.py
@@ -16,7 +16,7 @@ from .tensor_model import TensorToTensorRegressionModel, TensorToScalarRegressio
     TensorToTensorClassificationModel, TensorToScalarClassificationModel
 from .vector_model import VectorModelBase, VectorModel, VectorRegressionModel, VectorClassificationModel
 
-__version__ = "0.1.7.dev0"
+__version__ = "0.1.7"
 
 # The following submodules are not imported by default to avoid necessarily requiring their dependencies:
 # tensorflow
