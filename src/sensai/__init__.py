a957286fcc06b5cf51f72feef12d2526cf9d0af6
Mischa Panchenko <michael.panchenko@unternehmertum.de>
2022-07-01 16:34:10 +0200

    Fixed version strings

--- src/sensai/__init__.py
+++ src/sensai/__init__.py
@@ -16,7 +16,7 @@ from .tensor_model import TensorToTensorRegressionModel, TensorToScalarRegressio
     TensorToTensorClassificationModel, TensorToScalarClassificationModel
 from .vector_model import VectorModelBase, VectorModel, VectorRegressionModel, VectorClassificationModel
 
-__version__ = "0.1.6"
+__version__ = "0.1.8.dev1"
 
 # The following submodules are not imported by default to avoid necessarily requiring their dependencies:
 # tensorflow
