47f0ea8504d763a38418f1ff9983466d97cc1e2c
Dominik Jain <dominik.jain@outlook.com>
2024-08-12 18:12:36 +0200

    Further sklearn version checks in compatibility test

--- tests/backwardscompat/test_models.py
+++ tests/backwardscompat/test_models.py
@@ -25,11 +25,16 @@ def test_regression_model_backward_compatibility_v0_2_0(pickle_file, diabetesReg
     Tests for compatibility with models created with v0.2.0 using create_test_models.py
     """
     sklearn_version = Version(sklearn)
+
     if not sklearn_version.is_at_most(1, 0, 2):
         # NOTE: sklearn is breaking persisted objects with newer versions (e.g. OneHotEncoder),
         # so we can only run this test with the correct sklearn version
         pytest.skip("This test is only compatible with sklearn <= 1.0.2")
+
+    if "RandomForest" in pickle_file:
+        if not sklearn_version.is_equal(1, 0, 2):
+            pytest.skip("This test is only compatible with sklearn == 1.0.2")
+
     d = load_pickle(pickle_file)
     r2, model = d["R2"], d["model"]
     diabetesRegressionTestCase.testMinR2(model, r2-0.02, fit=False)
-
