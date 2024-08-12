1afd00caeef11e995e43fae4eaa88c025cb13fcd
Dominik Jain <dominik.jain@outlook.com>
2024-08-12 17:55:09 +0200

    test_regression_model_backward_compatibility_v0_2_0: Add check for correct sklearn version

--- tests/backwardscompat/test_models.py
+++ tests/backwardscompat/test_models.py
@@ -2,11 +2,13 @@ import os
 from glob import glob
 
 import pytest
+import sklearn
 
 import sensai
 from sensai import VectorModel
 from sensai.util.pickle import load_pickle
 from model_test_case import RESOURCE_DIR
+from sensai.util.version import Version
 
 
 def test_classification_model_backward_compatibility_v0_0_4(testResources, irisClassificationTestCase):
@@ -17,12 +19,16 @@ def test_classification_model_backward_compatibility_v0_0_4(testResources, irisC
     irisClassificationTestCase.testMinAccuracy(model, 0.8, fit=False)
 
 
-# TODO NOTE sklearn is breaking persisted objects with newer versions (e.g. OneHotEncoder), so this will ultimately need to use entirely different models
 @pytest.mark.parametrize("pickle_file", glob(f"{RESOURCE_DIR}/backward_compatibility/regression_model_*.v0.2.0.pickle"))
 def test_regression_model_backward_compatibility_v0_2_0(pickle_file, diabetesRegressionTestCase):
     """
     Tests for compatibility with models created with v0.2.0 using create_test_models.py
     """
+    sklearn_version = Version(sklearn)
+    if not sklearn_version.is_at_most(1, 0, 2):
+        # NOTE: sklearn is breaking persisted objects with newer versions (e.g. OneHotEncoder),
+        # so we can only run this test with the correct sklearn version
+        pytest.skip("This test is only compatible with sklearn <= 1.0.2")
     d = load_pickle(pickle_file)
     r2, model = d["R2"], d["model"]
     diabetesRegressionTestCase.testMinR2(model, r2-0.02, fit=False)
