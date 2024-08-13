b158ef0775ca109d21b4d2ae3a334f686887f428
Dominik Jain <dominik.jain@outlook.com>
2024-08-13 14:58:37 +0200

    Account for PYTHONPATH not being set

--- build_scripts/pythonpath.py
+++ build_scripts/pythonpath.py
@@ -2,4 +2,4 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.realpath(__file__))
 
-print(os.environ["PYTHONPATH"] + os.pathsep + os.path.abspath(os.path.join(SCRIPT_DIR, "..", "src")))
+print(os.environ.get("PYTHONPATH", "") + os.pathsep + os.path.abspath(os.path.join(SCRIPT_DIR, "..", "src")))
